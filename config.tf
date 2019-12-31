72456a613662172cd9475feff0f4affccb141b51
suaaa7 <spuaaa7@gmail.com>
2019-12-31 22:04:28 +0900

    Format config.tf

--- config.tf
+++ config.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.12.6"
 
   required_providers {
-    archive  = "~> 1.3.0"
-    aws      = "~> 2.43.0"
+    archive = "~> 1.3.0"
+    aws     = "~> 2.43.0"
   }
 }
 
